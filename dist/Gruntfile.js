ada56d7c5d077df52b858a2addc46d7cbe1fd254
whwnow <whwnow@gmail.com>
2014-03-06 13:03:47 +0800

    打包0.10.2版本

--- dist/Gruntfile.js
+++ dist/Gruntfile.js
@@ -1,31 +1,27 @@
 module.exports = function(grunt) {
-
-    // 项目配置
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> by LiTao */\n'
-            },
-            dist: {
-                files: {
-                    './public/javascripts/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-                }
-            }
-        },
-        concat: {
-            dist: {
-                src: [ "../src/units.js","../src/sagyChart.js"],
-                dest: "./public/javascripts/<%= pkg.name %>.js"
-            }
+  // 项目配置
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> by LiTao */\n'
+      },
+      dist: {
+        files: {
+          './public/javascripts/<%= pkg.name %>.min.js': ['./public/javascripts/<%= pkg.name %>.src.js']
         }
-    });
-
-    // 加载提供"uglify"任务的插件
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    // 默认任务
-    grunt.registerTask('default', ['concat', 'uglify']);
+      }
+    },
+    concat: {
+      dist: {
+        src: ["../src/sagyChart.js"],
+        dest: "./public/javascripts/<%= pkg.name %>.src.js"
+      }
+    }
+  });
+  // 加载提供"uglify"任务的插件
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  // 默认任务
+  grunt.registerTask('default', ['concat', 'uglify']);
 };
