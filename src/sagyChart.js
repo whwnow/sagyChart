42dc4a90d6da0643225483fbad22f7cfe4bfdf59
whwnow <whwnow@gmail.com>
2014-04-01 12:17:50 +0800

    打包 0.11.5版本

--- src/sagyChart.js
+++ src/sagyChart.js
@@ -9,7 +9,7 @@
   }
 }).call(this, 'sagyChart', function() {
   //some global variable
-  var im_version = '0.11.4',
+  var im_version = '0.11.5',
     im_obj = {},
     im_string = im_obj.toString,
     // im_hasOwn = im_obj.hasOwnProperty,
