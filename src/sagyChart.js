44e376ee61dd0f5ff13e3f9e436241a616945c63
whwnow <whwnow@gmail.com>
2014-04-08 11:24:40 +0800

    单位转换添加字符串返回值,去掉单位name注释

--- src/sagyChart.js
+++ src/sagyChart.js
@@ -30,217 +30,181 @@
     document = root.document,
     units = root.unitDocs = {
       'Wh': {
-        //name: '瓦时',
         lowerLevel: null,
         higherLevel: 'kWh',
         ratio: 1000
       },
       'kWh': {
-        //name: '千瓦时',
         lowerLevel: 'Wh',
         higherLevel: 'MWh',
         ratio: 1000
       },
       'MWh': {
-        //name: '兆瓦时',
         lowerLevel: 'kWh',
         higherLevel: 'GWh',
         ratio: 1000
       },
       'GWh': {
-        //name: '吉瓦时',
         lowerLevel: 'MWh',
         higherLevel: 'TWh',
         ratio: 1000
       },
       'TWh': {
-        //name: '太瓦时',
         lowerLevel: 'GWh',
         higherLevel: 'PWh',
         ratio: 1000
       },
       'PWh': {
-        //name: '拍瓦时',
         lowerLevel: 'TWh',
         higherLevel: 'EWh',
         ratio: 1000
       },
       'EWh': {
-        //name: '艾瓦时',
         lowerLevel: 'PWh',
         higherLevel: 'ZWh',
         ratio: 1000
       },
       'ZWh': {
-        //name: '泽瓦时',
         lowerLevel: 'EWh',
         higherLevel: 'YWh',
         ratio: 1000
       },
       'YWh': {
-        //name: '尧瓦时',
         lowerLevel: 'ZWh',
         higherLevel: null,
         ratio: 1000
       },
       'W': {
-        //name: '瓦',
         lowerLevel: null,
         higherLevel: 'kW',
         ratio: 1000
       },
       'kW': {
-        //name: '千瓦',
         lowerLevel: 'W',
         higherLevel: 'MW',
         ratio: 1000
       },
       'MW': {
-        //name: '兆瓦',
         lowerLevel: 'kW',
         higherLevel: 'GW',
         ratio: 1000
       },
       'GW': {
-        //name: '吉瓦',
         lowerLevel: 'MW',
         higherLevel: 'TW',
         ratio: 1000
       },
       'TW': {
-        //name: '太瓦',
         lowerLevel: 'GW',
         higherLevel: 'PW',
         ratio: 1000
       },
       'PW': {
-        //name: '拍瓦',
         lowerLevel: 'TW',
         higherLevel: 'EW',
         ratio: 1000
       },
       'EW': {
-        //name: '艾瓦',
         lowerLevel: 'PW',
         higherLevel: 'ZW',
         ratio: 1000
       },
       'ZW': {
-        //name: '泽瓦',
         lowerLevel: 'EW',
         higherLevel: 'YW',
         ratio: 1000
       },
       'YW': {
-        //name: '尧瓦',
         lowerLevel: 'ZW',
         higherLevel: null,
         ratio: 1000
       },
       '元': {
-        //name: '元',
         lowerLevel: null,
         higherLevel: '万元',
         ratio: 10000
       },
       '万元': {
-        //name: '万元',
         lowerLevel: '元',
         higherLevel: '亿元',
         ratio: 10000
       },
       '亿元': {
-        //name: '亿元',
         lowerLevel: '万元',
         higherLevel: null,
         ratio: 10000
       },
       'g': {
-        //name: '克',
         lowerLevel: null,
         higherLevel: 'kg',
         ratio: 1000
       },
       'kg': {
-        //name: '千克',
         lowerLevel: 'g',
         higherLevel: 'T',
         ratio: 1000
       },
       'T': {
-        //name: '吨',
         lowerLevel: 'kg',
         higherLevel: 'kT',
         ratio: 1000
       },
       'kT': {
-        //name: '千吨',
         lowerLevel: 'T',
         higherLevel: 'MT',
         ratio: 1000
       },
       'MT': {
-        //name: '千吨',
         lowerLevel: 'kT',
         higherLevel: null,
         ratio: 1000
       },
       '克': {
-        //name: '克',
         lowerLevel: null,
         higherLevel: '千克',
         ratio: 1000
       },
       '千克': {
-        //name: '千克',
         lowerLevel: '克',
         higherLevel: '吨',
         ratio: 1000
       },
       '吨': {
-        //name: '吨',
         lowerLevel: '千克',
         higherLevel: '千吨',
         ratio: 1000
       },
       '千吨': {
-        //name: '千吨',
         lowerLevel: '吨',
         higherLevel: '兆吨',
         ratio: 1000
       },
       '兆吨': {
-        //name: '千吨',
         lowerLevel: '千吨',
         higherLevel: null,
         ratio: 1000
       },
       'J': {
-        //name: '焦',
         lowerLevel: null,
         higherLevel: 'kJ',
         ratio: 1000
       },
       'kJ': {
-        //name: '千焦',
         lowerLevel: 'J',
         higherLevel: 'MJ',
         ratio: 1000
       },
       'MJ': {
-        //name: '兆焦',
         lowerLevel: 'kJ',
         higherLevel: null,
         ratio: 1000
       },
       'L': {
-        //name: '升',
         lowerLevel: null,
         higherLevel: 'T',
         ratio: 1000
       },
       'm³': {
-        //name: '立方米',
         lowerLevel: 'L',
         higherLevel: null,
         ratio: 1000
@@ -1031,7 +995,7 @@
       unit: convertUnit
     };
   };
-  sagyChart.fn.convertUnit = sagyChart.convertUnit = function(value, baseUnit) {
+  sagyChart.fn.convertUnit = sagyChart.convertUnit = function(value, baseUnit, returnNum) {
     var convertUnit = baseUnit,
       baseUnitObj = units[baseUnit],
       len = value < 10 ? -1 : 0,
@@ -1061,7 +1025,7 @@
     }
     temp = value * mathPow(ratio, len * -1);
     return {
-      data: numFormat(temp, true),
+      data: numFormat(temp, returnNum),
       unit: convertUnit
     };
   };
