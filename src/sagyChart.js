820c1fcd35a133fd3474dee6041a6665a8f33fb9
whwnow <whwnow@gmail.com>
2014-04-21 16:54:57 +0800

    升级到0.12.3

--- src/sagyChart.js
+++ src/sagyChart.js
@@ -9,7 +9,7 @@
   }
 }).call(this, 'sagyChart', function() {
   //some global variable
-  var im_version = '0.12.2',
+  var im_version = '0.12.3',
     im_obj = {},
     im_string = im_obj.toString,
     // im_hasOwn = im_obj.hasOwnProperty,
